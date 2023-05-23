3fa9307de2db9886854b9703b58320e0c961c83b
Kenji1504 <gamboakenleam1015@gmail.com>
2023-05-23 23:09:19 +0800

    redo the main program

--- calculator_main.py
+++ calculator_main.py
@@ -2,14 +2,8 @@
 # BSCPE 1-5
 # Creating an object-oriented calculator program
 
-from user_interface import UserInterface
 from calculator_object import Calculator
-user_interface = UserInterface()
 calculator = Calculator()
 
-# ask for user's input
-num_1 = user_interface.user_input()
-num_2 = user_interface.user_input_2()
-# ask for mathematical operator
-math_operator = calculator.math_operator(num_1, num_2)
-# create a method that can loop the program
+# call out the loop method
+calculator.loop_calculator()
