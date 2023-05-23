eadbaca6839a96c3164c031245ba605f97746fc5
Kenji1504 <gamboakenleam1015@gmail.com>
2023-05-23 20:38:05 +0800

    Pseudocode

---
+++ calculator_main.py
@@ -0,0 +1,10 @@
+# Ken Leam G. Gamboa
+# BSCPE 1-5
+# Creating an object-oriented calculator program
+
+# Pseudocode:
+# create a class named 'Calculator'
+# create a method that asks for user input
+# create a method to determine what mathematical operator will be used
+# create a method that can loop the program 
+# print the output   
