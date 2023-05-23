fdae2d3c54b8b56db1fbe0890464dad911b9eef0
Kenji1504 <gamboakenleam1015@gmail.com>
2023-05-23 21:50:58 +0800

    to input second value

--- calculator_main.py
+++ calculator_main.py
@@ -8,6 +8,8 @@ user_interface = UserInterface()
 # ask for user's input
 num_1 = user_interface.user_input()
 print(str(num_1))
+num_2 = user_interface.user_input_2()
+print(str(num_2))
 # create a class named 'Calculator'
 # create a method to determine what mathematical operator will be used
 # create a method that can loop the program 
