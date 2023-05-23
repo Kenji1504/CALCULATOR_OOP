d51023b0822ed94d4ffdc032f98c7b5e7a3d69e3
Kenji1504 <gamboakenleam1015@gmail.com>
2023-05-23 21:47:31 +0800

    to input first value

--- calculator_main.py
+++ calculator_main.py
@@ -2,9 +2,13 @@
 # BSCPE 1-5
 # Creating an object-oriented calculator program
 
-# Pseudocode:
+from user_interface import UserInterface
+user_interface = UserInterface()
+
+# ask for user's input
+num_1 = user_interface.user_input()
+print(str(num_1))
 # create a class named 'Calculator'
-# create a method that asks for user input
 # create a method to determine what mathematical operator will be used
 # create a method that can loop the program 
 # print the output   
---
+++ user_interface.py
@@ -0,0 +1,16 @@
+
+# create a class named 'UserInterface'
+class UserInterface:
+# create a method that asks for user input
+    def user_input(self):
+         # create a while loop
+        while True:
+	        # try asking the user to input two numerical values.
+            try:
+                value_input = float(input("\nInput the first value: "))
+                # break the loop if inputs are valid
+                return value_input
+	        # use except function to capture any Value Error
+            except ValueError:
+                print("\033[31m" + "\nInvalid Input, input numerical characters only.\n")
+                print("\033[95m" + "Please try again.\n")
