d76ca3d45a4ee5e772760b544958d6b1c12bff28
Kenji1504 <gamboakenleam1015@gmail.com>
2023-06-03 19:48:15 +0800

    adding upgraded calculator class

---
+++ calculator_upgrade.py
@@ -0,0 +1,9 @@
+# Ken Leam G. Gamboa
+# BSCPE 1-5
+# Inheriting the Calculator class, and adding other features such as: Square of a number,
+# Exponentiation, Square root, and Pythagorean Theorem 
+
+from calculator_object import Calculator
+
+class UpgradedCalculator(Calculator):
+    pass
